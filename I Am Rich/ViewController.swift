347c151cbb7481a5b6963645354b1bc78349c6a2
MariaSyed <mariasy@metropolia.fi>
2017-12-09 19:38:00 +0200

    Center align text and image

--- I Am Rich/ViewController.swift
+++ I Am Rich/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var diamond: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        label.textAlignment = .center
+        diamond.frame.origin.x = (self.view.bounds.size.width - diamond.frame.size.width) / 2.0 // centered left to right
     }
 
     override func didReceiveMemoryWarning() {
